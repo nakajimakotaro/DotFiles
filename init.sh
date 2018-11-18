68ecf38addc7d3ea1f7ddb2605f2ffe74f5d7b79
nakajimakotaro <harusann2@gmail.com>
2018-11-18 19:25:15 +0900

    ルートで実行したときエラーを出すよう修正

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+if [ $UID = 0 ]; then
+    echo "Rootで実行しないでください"
+    exit
+fi
+
 cd $(dirname $0)
 
 if ! ( which git && which fish ); then
     pacman -Syu --noconfirm git fish
 fi
 
-config/deploy.fish
 install/install.fish
+config/deploy.fish
 
