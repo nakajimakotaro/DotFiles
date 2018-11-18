bc641dd9f1234abd87ae8bc4aafda3cbd902b7c9
nakajimakotaro <harusann2@gmail.com>
2018-11-18 19:10:03 +0900

    yayでインストールするスクリプトを作成

--- install/install.fish
+++ install/install.fish
@@ -1,1 +1,9 @@
 #!/usr/bin/fish
+
+cd (dirname (status -f))
+
+curl -L https://github.com/Jguer/yay/releases/download/v8.1173.0/yay_8.1173.0_x86_64.tar.gz | tar xz --strip-components=1 --no-anchored yay
+./yay -Syu --noconfirm yay-bin
+rm ./yay
+
+yay -Syu --noconfirm - < package.list
